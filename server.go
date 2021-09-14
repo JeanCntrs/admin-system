c0cf2daea105fcec50da7bb74762e982971d4c2d
Jean C. Contreras <jean.cntrs@gmail.com>
2021-09-13 23:44:04 -0300

    Were handled parameters that arrive from the url

--- server.go
+++ server.go
@@ -12,6 +12,17 @@ func main() {
 
 	http.HandleFunc("/products", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Products page")
+
+		mapURL := r.URL.Query()
+		fmt.Println("mapURL", mapURL)
+
+		mapURL.Add("Price", "10")
+		fmt.Println("mapURL.Encode()", mapURL.Encode())
+
+		fmt.Println("URL", r.URL)
+		fmt.Println("RawQuery", r.URL.RawQuery)
+		fmt.Println("Name", r.URL.Query().Get("name"))
+		fmt.Println("ProductId", r.URL.Query().Get("productId"))
 	})
 
 	http.HandleFunc("/categories", func(w http.ResponseWriter, r *http.Request) {
