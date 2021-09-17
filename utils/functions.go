51a8d47cab98705c27a135caed3d15f34710e3a5
Jean C. Contreras <jean.cntrs@gmail.com>
2021-09-16 23:48:28 -0300

    Was added function send request

--- utils/functions.go
+++ utils/functions.go
@@ -1,6 +1,11 @@
 package utils
 
-import "net/url"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
 
 // GenerateURL generates a url dynamically
 func GenerateURL(uri, host, protocol string, urlParams map[string]string) string {
@@ -16,4 +21,28 @@ func GenerateURL(uri, host, protocol string, urlParams map[string]string) string
 	url.RawQuery = mapFunction.Encode()
 
 	return url.String()
+}
+
+// SendRequest send a request dynamically
+func SendRequest() {
+	url := GenerateURL("/products", "localhost:8000", "http", nil)
+	request, requestErr := http.NewRequest("GET", url, nil)
+	if requestErr != nil {
+		panic("There was a problem with the request")
+	}
+
+	client := &http.Client{}
+	response, responseErr := client.Do(request)
+	if responseErr != nil {
+		fmt.Println("responseErr", responseErr)
+		panic("There was a problem with the client")
+	}
+
+	bytes, readAllErr := ioutil.ReadAll(response.Body)
+	if readAllErr != nil {
+		panic("There was a problem reading the request body")
+	}
+
+	fmt.Println("Status:", response.Status)
+	fmt.Println("Body:", string(bytes))
 }
