84c025bcbd2d9c07f4b4cc5651555a1d2780adff
Jean C. Contreras <jean.cntrs@gmail.com>
2021-09-21 23:52:44 -0300

    Was added the use of loops in the template

--- server.go
+++ server.go
@@ -14,18 +14,12 @@ func main() {
 	})
 
 	http.HandleFunc("/products", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Products page")
+		friends := []string{"Marcela", "Laura", "Constanza", "Camila"}
+		person := Person{PersonID: 1, Names: "Jean Carlos", Surnames: "Contreras Contreras", Age: 27, IsMan: true, Friends: friends}
 
-		mapURL := r.URL.Query()
-		fmt.Println("mapURL", mapURL)
+		template := template.Must(template.ParseFiles("./html/product.html"))
 
-		mapURL.Add("Price", "10")
-		fmt.Println("mapURL.Encode()", mapURL.Encode())
-
-		fmt.Println("URL", r.URL)
-		fmt.Println("RawQuery", r.URL.RawQuery)
-		fmt.Println("Name", r.URL.Query().Get("name"))
-		fmt.Println("ProductId", r.URL.Query().Get("productId"))
+		template.Execute(w, person)
 	})
 
 	http.HandleFunc("/categories", func(w http.ResponseWriter, r *http.Request) {
@@ -59,4 +53,5 @@ type Person struct {
 	Surnames string
 	Age      int
 	IsMan    bool
+	Friends  []string
 }
