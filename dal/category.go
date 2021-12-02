4c0e7da17987531921fe908f052f4218209ebd89
Jean C. Contreras <jean.cntrs@gmail.com>
2021-12-01 22:56:53 -0300

    Was added function to filter categories

--- dal/category.go
+++ dal/category.go
@@ -5,15 +5,29 @@ import (
 	"github.com/JeanCntrs/admin-system/models"
 )
 
-type CategoryList []models.Category
-
-func ListCategories() CategoryList {
+func ListCategories() []models.Category {
 	query := `SELECT * FROM uspListCategories()`
 	database.OpenConnection()
 	rows, _ := database.Query(query)
 	database.CloseConnection()
 
-	categoryList := CategoryList{}
+	categoryList := []models.Category{}
+	for rows.Next() {
+		category := models.Category{}
+		rows.Scan(&category.Idcategoria, &category.Nombre, &category.Descripcion)
+		categoryList = append(categoryList, category)
+	}
+
+	return categoryList
+}
+
+func FilterCategories(searchParam string) []models.Category {
+	query := `SELECT * FROM uspFilterCategories($1)`
+	database.OpenConnection()
+	rows, _ := database.Query(query, searchParam)
+	database.CloseConnection()
+
+	categoryList := []models.Category{}
 	for rows.Next() {
 		category := models.Category{}
 		rows.Scan(&category.Idcategoria, &category.Nombre, &category.Descripcion)
--- handlers/category.go
+++ handlers/category.go
@@ -4,11 +4,21 @@ import (
 	"net/http"
 
 	"github.com/JeanCntrs/admin-system/dal"
+	"github.com/JeanCntrs/admin-system/models"
 	"github.com/JeanCntrs/admin-system/utils"
 )
 
 func Category(w http.ResponseWriter, r *http.Request) {
-	categoryList := dal.ListCategories()
+	var categories = []models.Category{}
 
-	utils.RenderTemplate(w, "category", categoryList)
+	if r.Method == "GET" {
+		categories = dal.ListCategories()
+	}
+
+	if r.Method == "POST" {
+		searchParam := r.FormValue("categoryName")
+		categories = dal.FilterCategories(searchParam)
+	}
+
+	utils.RenderTemplate(w, "category", categories)
 }
