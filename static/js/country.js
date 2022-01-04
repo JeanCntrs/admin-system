c85e61522cc685cf6cc88781957fbac72726bcc1
Jean C. Contreras <jean.cntrs@gmail.com>
2022-01-04 20:18:08 -0300

    Were added dynamically fields in countries table

--- static/js/country.js
+++ static/js/country.js
@@ -2,26 +2,7 @@ window.onload = () => {
     const url = '/countries/list';
     const tableHeaders = ['Country ID', 'Name', 'Capital'];
     const elementId = 'country_table'
+    const fields = ['CountryId', 'Name', 'Capital'];
 
-    getData(url, tableHeaders, elementId);
-}
-
-const getData = (url, tableHeaders, elementId) => {
-    let table = '<table id="table" class="table">';
-
-    fetch(url)
-        .then(response => response.json())
-        .then(response => {
-            table += '<thead class="table-dark">';
-            table += '<tr>';
-
-            tableHeaders.forEach(header => {
-                table += `<th>${header}</th>`;
-            });
-
-            table += '</tr>';
-            table += '</thead>';
-            table += '</table>';
-            document.getElementById(elementId).innerHTML = table;
-        })
+    getData(url, tableHeaders, fields, elementId);
 }
--- static/js/main.js
+++ static/js/main.js
@@ -23,3 +23,38 @@ const confirmation = (title = 'Are you sure?', text = 'If you are sure confirm t
         allowOutsideClick: false
     });
 }
+
+const getData = (url, tableHeaders, fields, elementId) => {
+    let table = '<table id="table" class="table">';
+
+    fetch(url)
+        .then(response => response.json())
+        .then(response => {
+            table += '<thead class="table-dark">';
+            table += '<tr>';
+
+            tableHeaders.forEach(header => {
+                table += `<th>${header}</th>`;
+            });
+
+            table += '</tr>';
+            table += '</thead>';
+            table += '<tbody>';
+
+            response.forEach(element => {
+                table += '<tr>';
+
+                fields.forEach(field => {
+                    table += '<td>';
+                    table += element[field]
+                    table += '</td>';
+                });
+
+                table += '</tr>';
+            });
+
+            table += '</tbody>';
+            table += '</table>';
+            document.getElementById(elementId).innerHTML = table;
+        })
+}
